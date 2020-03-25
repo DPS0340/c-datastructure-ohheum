6766fca859b53a11b2e936dc84c919f9d11550f5
jiho lee <jiholee@kpu.ac.kr>
2020-03-25 22:33:27 +0900

    섹션 1 끝

--- src/1/3.c
+++ src/1/3.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void) {
-    int* array = (int*)malloc(4*siszeof(int));
+    int* array = (int*)malloc(4*sizeof(int));
+    // int array[4]로 선언할 시
+    // 주소값 변경 불가
+
+    if(!array) {
+        exit(1);
+    }
 
     array[0] = 1;
     array[1] = 2;
     *(array+2)= 3;
 
     int* tmp = (int*)malloc(8*sizeof(int));
+    if(!array) {
+        exit(1);
+    }
     for(int i=0;i<4;i++) {
         tmp[i] = array[i];
     }
+
+    free(array);
+
+    array = tmp;
+    
+    array[4] = 4;
+    array[5] = 5;
+
+    printf("%d", array[5]);
 
     return 0;
 }
