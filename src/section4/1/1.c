5ffea9556e9876a2e5229db4eaacb56165cf8afb
jiho lee <jiholee@kpu.ac.kr>
2020-03-27 14:33:02 +0900

    section 4

---
+++ src/section4/1/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CAPACITY 100
+#define BUFFER_SIZE 20
+
+char* names[CAPACITY];
+char* number[CAPACITY];
+int n = 0;
+
+void add();
+void find();
+void status();
+void remove();
+
